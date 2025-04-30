b33dc465d49140309ba4f2c24cec453d60e85cca
helijacky <helijackybu@gmail.com>
2025-04-30 13:47:55 +1000

    Update index.js
    test

--- functions/index.js
+++ functions/index.js
@@ -10,15 +10,6 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-
 const functions = require("firebase-functions");
 const express = require("express");
 const {userController} = require('./controller/userController');
